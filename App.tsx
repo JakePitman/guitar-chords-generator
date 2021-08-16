ce7f1f4c16a76980b2095efb8268ccf1fb73ae58
Jay Pitman <jakeseanp@gmail.com>
2021-08-16 23:29:10 +1000

    Add basic tab navigation

--- App.tsx
+++ App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 import MainScreen from "./screens/MainScreen"
+import ChordsScreen from "./screens/ChordsScreen"
+import BeatScreen from "./screens/BeatScreen"
+
+const TabNavigator = createBottomTabNavigator({
+  Main: MainScreen,
+  Chords: ChordsScreen,
+  Beat: BeatScreen,
+});
+
+const Navigator = createAppContainer(TabNavigator)
 
 export default function App() {
   return (
-    <MainScreen/>
+    <Navigator/>
   )
 }
