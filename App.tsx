7baf66d086c372645d18d5933806c6c0356a06bd
Jay Pitman <jakeseanp@gmail.com>
2021-08-16 23:42:59 +1000

    Add icons to tabs

--- App.tsx
+++ App.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 
@@ -7,9 +10,24 @@ import ChordsScreen from "./screens/ChordsScreen"
 import BeatScreen from "./screens/BeatScreen"
 
 const TabNavigator = createBottomTabNavigator({
-  Main: MainScreen,
-  Chords: ChordsScreen,
-  Beat: BeatScreen,
+  Main: { 
+    screen: MainScreen,
+    navigationOptions: {
+      tabBarIcon: <FontAwesome5 name="guitar" size={24} color="black" /> 
+    }
+  },
+  Chords: { 
+    screen: ChordsScreen,
+    navigationOptions: {
+      tabBarIcon: <FontAwesome name="th-list" size={24} color="black" />
+    }
+  },
+  Beat: { 
+    screen: BeatScreen,
+    navigationOptions: {
+      tabBarIcon: <Ionicons name="musical-note" size={24} color="black" />
+    }
+  },
 });
 
 const Navigator = createAppContainer(TabNavigator)
