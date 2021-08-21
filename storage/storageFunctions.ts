66f8e3c2635f4539cc9a4c7985217d9e369be726
Jay Pitman <jakeseanp@gmail.com>
2021-08-21 21:02:15 +1000

    Add default values to retrieveValue

--- storage/storageFunctions.ts
+++ storage/storageFunctions.ts
@@ -1,29 +1,50 @@
 import { AsyncStorage } from "react-native";
 
-const bpmKey = 'MY_BPM';
-const finalBeatKey = 'MY_FINAL_BEAT';
-const selectedChordsKey = 'MY_SELECTED_CHORDS';
+const bpmKey = "MY_BPM";
+const finalBeatKey = "MY_FINAL_BEAT";
+const selectedChordsKey = "MY_SELECTED_CHORDS";
 
-type ValueType =
-  'BPM' |
-  'FINAL_BEAT' |
-  'SELECTED_CHORDS'
+type ValueType = "BPM" | "FINAL_BEAT" | "SELECTED_CHORDS";
 
 export const storeValue = async (valueType: ValueType, value: string) => {
   try {
     await AsyncStorage.setItem(valueType, value);
   } catch (error) {
-    console.log(`Error storing value: ${value} to value type: ${valueType}`, {error})
+    console.log(`Error storing value: ${value} to value type: ${valueType}`, {
+      error,
+    });
   }
-}
+};
 
 export const retrieveValue = async (valueType: ValueType) => {
   try {
     const value = await AsyncStorage.getItem(valueType);
-    if (value !== null) {
-      return value
+    if (value) {
+      return value;
+    } else {
+      // Default values
+      switch (valueType) {
+        case "BPM":
+          return "60";
+        case "FINAL_BEAT":
+          return "4";
+        case "SELECTED_CHORDS":
+          return "";
+      }
     }
-    } catch (error) {
-      console.log(`Error retrieving value from value type: ${valueType}`, {error})
-    }
-}
+  } catch (error) {
+    console.log(`Error retrieving value from value type: ${valueType}`, {
+      error,
+    });
+  }
+};
+
+export const initValues = async () => {
+  const bpm = await retrieveValue("BPM");
+  const finalBeat = await retrieveValue("FINAL_BEAT");
+  const selectedChords = await retrieveValue("SELECTED_CHORDS");
+
+  !bpm && storeValue("BPM", "my default bpm");
+  !finalBeat && storeValue("FINAL_BEAT", "my default final beat");
+  !selectedChords && storeValue("SELECTED_CHORDS", "my default chords");
+};
