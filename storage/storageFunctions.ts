bcaa96c3dfbc399f2057d05f14bfe3f1b5b77467
Jay Pitman <jakeseanp@gmail.com>
2021-08-21 20:36:58 +1000

    Make storage/ retrieval funcs take args

--- storage/storageFunctions.ts
+++ storage/storageFunctions.ts
@@ -4,21 +4,26 @@ const bpmKey = 'MY_BPM';
 const finalBeatKey = 'MY_FINAL_BEAT';
 const selectedChordsKey = 'MY_SELECTED_CHORDS';
 
-export const storeBpm = async () => {
+type ValueType =
+  'BPM' |
+  'FINAL_BEAT' |
+  'SELECTED_CHORDS'
+
+export const storeValue = async (valueType: ValueType, value: string) => {
   try {
-    await AsyncStorage.setItem(bpmKey, '4');
+    await AsyncStorage.setItem(valueType, value);
   } catch (error) {
-    console.log({error})
+    console.log(`Error storing value: ${value} to value type: ${valueType}`, {error})
   }
 }
 
-export const retrieveBpm = async () => {
+export const retrieveValue = async (valueType: ValueType) => {
   try {
-    const value = await AsyncStorage.getItem(bpmKey);
+    const value = await AsyncStorage.getItem(valueType);
     if (value !== null) {
       return value
     }
     } catch (error) {
-      console.log({error})
+      console.log(`Error retrieving value from value type: ${valueType}`, {error})
     }
 }
