488a9b313fd83a2175f9f633e847040409673841
Jay Pitman <jakeseanp@gmail.com>
2021-08-22 14:47:18 +1000

    Move TabNavigator into own component

--- App.tsx
+++ App.tsx
@@ -1,17 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
-import { FontAwesome5 } from "@expo/vector-icons";
-import { Ionicons } from "@expo/vector-icons";
-import { FontAwesome } from "@expo/vector-icons";
-import { createAppContainer } from "react-navigation";
-import { createBottomTabNavigator } from "react-navigation-tabs";
 
-import MainScreen from "./screens/MainScreen";
-import ChordsScreen from "./screens/ChordsScreen";
-import BeatScreen from "./screens/BeatScreen";
-import { Colors } from "./shared/styles";
 import Chords from "./shared/chords";
 import { retrieveValue } from "./storage/storageFunctions";
+import Navigator from "./components/TabNavigator";
 
 type ScreenProps = {
   settings: {
@@ -20,47 +12,6 @@ type ScreenProps = {
     selectedChordsData: { name: string; path: number }[];
   };
 };
-
-const TabNavigator = createBottomTabNavigator(
-  {
-    Main: {
-      screen: ({ screenProps }: any) => {
-        return <MainScreen settings={screenProps.settings} />;
-      },
-      navigationOptions: {
-        tabBarIcon: ({ focused, tintColor }) => (
-          <FontAwesome5 name="guitar" size={24} color={tintColor} />
-        ),
-      },
-    },
-    Chords: {
-      screen: ChordsScreen,
-      navigationOptions: {
-        tabBarIcon: ({ focused, tintColor }) => (
-          <FontAwesome name="th-list" size={24} color={tintColor} />
-        ),
-      },
-    },
-    Beat: {
-      screen: BeatScreen,
-      navigationOptions: {
-        tabBarIcon: ({ focused, tintColor }) => (
-          <Ionicons name="musical-note" size={24} color={tintColor} />
-        ),
-      },
-    },
-  },
-  {
-    tabBarOptions: {
-      activeBackgroundColor: Colors.brown,
-      inactiveBackgroundColor: Colors.lightBrown,
-      activeTintColor: Colors.beige,
-      inactiveTintColor: Colors.brown,
-    },
-  }
-);
-
-const Navigator = createAppContainer(TabNavigator);
 
 const filterChords = (selectedChordsString: string) => {
   const selectedChordNames = selectedChordsString.split(",");
