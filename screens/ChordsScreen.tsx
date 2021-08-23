f8452194705452b3755aa02c5ccf20336f0eae41
Jay Pitman <jakeseanp@gmail.com>
2021-08-23 22:31:52 +1000

    Scrollview Chords

--- screens/ChordsScreen.tsx
+++ screens/ChordsScreen.tsx
@@ -1,21 +1,39 @@
-import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import React from "react";
+import { StyleSheet, View, Text, ScrollView, Image } from "react-native";
 
-const ChordScreen = () => {
+import Chords from "../shared/chords";
+
+type Props = {
+  selectedChords: {
+    name: string;
+    path: number;
+  }[];
+};
+
+const ChordScreen = ({ selectedChords }: Props) => {
   return (
     <View style={styles.container}>
-      <Text>Chord Screen</Text>
+      <ScrollView>
+        {Chords.map((chord) => (
+          <Image style={styles.chord} source={chord.path} />
+        ))}
+      </ScrollView>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
     justifyContent: "center",
-    alignItems: "center"
-  }
-})
+    alignItems: "center",
+  },
+  chord: {
+    height: 150,
+    width: 130,
+    borderColor: "black",
+    borderWidth: 1,
+  },
+});
 
-export default ChordScreen
-
+export default ChordScreen;
