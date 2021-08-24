40ae2d8511ae9b0cbdbaf94b6e4d7652f99cc186
Jay Pitman <jakeseanp@gmail.com>
2021-08-24 23:38:36 +1000

    Add toggle button ui

--- screens/ChordsScreen.tsx
+++ screens/ChordsScreen.tsx
@@ -11,6 +11,19 @@ import {
 import Chords from "../shared/chords";
 import { Colors } from "../shared/styles";
 
+type ListItemProps = {
+  path: number;
+};
+
+const ListItem = ({ path }: ListItemProps) => {
+  return (
+    <View style={styles.listItemContainer}>
+      <Image style={styles.chord} source={path} />
+      <TouchableOpacity style={styles.chordToggleButton}></TouchableOpacity>
+    </View>
+  );
+};
+
 type Props = {
   selectedChords: {
     name: string;
@@ -24,9 +37,7 @@ const ChordScreen = ({ selectedChords }: Props) => {
       <FlatList
         data={Chords}
         keyExtractor={(item) => `${item.name}`}
-        renderItem={({ item }) => (
-          <Image style={styles.chord} source={item.path} />
-        )}
+        renderItem={({ item }) => <ListItem path={item.path} />}
         numColumns={3}
       />
       <TouchableOpacity style={styles.saveButtonContainer}>
@@ -44,11 +55,24 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: Colors.beige,
   },
+  listItemContainer: {
+    alignItems: "center",
+  },
   chord: {
     height: 110,
     width: 100,
     borderWidth: 1,
     margin: 10,
+  },
+  chordToggleButton: {
+    width: 30,
+    height: 30,
+    borderWidth: 1,
+    borderColor: Colors.brown,
+    borderRadius: 15,
+  },
+  chordToggleButtonActive: {
+    backgroundColor: Colors.brown,
   },
   saveButtonContainer: {
     width: "80%",
